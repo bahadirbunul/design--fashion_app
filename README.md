a984b9d7606de683264360480e791251bde74edd
Bahadır <bahadirbunul5@gmail.com>
2022-05-06 19:49:58 +0300

    design: fashion_app

---
+++ README.md
@@ -0,0 +1,17 @@
+# Design: fashion_app
+
+You can view my repo here 
+[Click Me](https://github.com/bahadirbunul/not_ortalamasi_hesaplayici)
+
+## Topics
+
+- Dart
+- Flutter
+
+## Author
+
+Bahadır Bünül (bahadirbunul)
+
+<img src="assets/ssfashion.jpeg">
+
+
---
+++ lib/detail.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class Detail extends StatefulWidget {
+  var imgPath;
+
+  Detail({Key? key, this.imgPath}) : super(key: key);
+
+  @override
+  State<Detail> createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Hero(
+            tag: widget.imgPath,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
+            ),
+          ),
+          Positioned(
+            left: 15,
+            right: 15,
+            bottom: 15,
+            child: Material(
+              borderRadius: BorderRadius.circular(10),
+              elevation: 4,
+              child: Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width - 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Row(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                            height: 120,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: Colors.grey),
+                                image: const DecorationImage(
+                                    image: const AssetImage('assets/dress.jpg'),
+                                    fit: BoxFit.contain))),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'LAMINATED',
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          const Text(
+                            'Louis Vuitton',
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            height: 30,
+                            width: MediaQuery.of(context).size.width - 170,
+                            child: const Text(
+                              'One button V-neck sling long-sleeved waist female stitching dress',
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 13,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ]),
+                    const Divider(),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15, top: 10, bottom: 2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            '\$6500',
+                            style: TextStyle(
+                                fontFamily: 'Montserrat', fontSize: 22),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 30),
+                            child: FloatingActionButton(
+                              child: const Center(
+                                child: Icon(
+                                  Icons.arrow_forward_ios,
+                                ),
+                              ),
+                              backgroundColor: Colors.brown, onPressed: () {  },
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
